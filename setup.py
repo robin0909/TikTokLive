493d41fd8c5bdf3cc5f1f9ad3871bd8c80e462c6
robinyang <robinyang.hubei@gmail.com>
2023-05-05 20:28:02 +0800

    feat(custom): upgrade custom version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="TikTokLiveCustom",
     packages=setuptools.find_packages(),
-    version="0.2",
+    version="0.3",
     license="MIT",
     description="TikTok Live Connection Client",
     author="robinyang",
